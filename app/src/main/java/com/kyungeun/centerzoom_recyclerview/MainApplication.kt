b019ba5137c2aba0de1fdc2999318dcf7e577788
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-07-22 22:08:48 +0900

    folder setting

---
+++ app/src/main/java/com/kyungeun/centerzoom_recyclerview/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.kyungeun.centerzoom_recyclerview
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+
+        }
+    }
+
+}
