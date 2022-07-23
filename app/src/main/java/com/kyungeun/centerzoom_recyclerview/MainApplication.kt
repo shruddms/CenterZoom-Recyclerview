474aec155991c1ee3c3eb85e32b9834d13d4c552
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-07-23 19:09:35 +0900

    gradle set

--- app/src/main/java/com/kyungeun/centerzoom_recyclerview/MainApplication.kt
+++ app/src/main/java/com/kyungeun/centerzoom_recyclerview/MainApplication.kt
@@ -2,13 +2,16 @@ package com.kyungeun.centerzoom_recyclerview
 
 import android.app.Application
 import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
+@HiltAndroidApp
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
-
+            Timber.plant(Timber.DebugTree())
         }
     }
 
